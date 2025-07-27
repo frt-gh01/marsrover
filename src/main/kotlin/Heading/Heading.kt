7e437102be93bb6aaa40c4d0da8f6d55f4f70a48
Franciscello <ftarulla+gh01@gmail.com>
2025-07-27 20:10:38 -0300

    Step25: Remove Heading hierarchy (bc repeated code)

--- src/main/kotlin/Heading/Heading.kt
+++ src/main/kotlin/Heading/Heading.kt
@@ -2,11 +2,12 @@ package dev.frtgh01.Heading
 
 import dev.frtgh01.Point2D
 
-interface Heading {
-    fun forwardPoint(): Point2D
-    fun backwardPoint(): Point2D
-    fun rightHeading(): Heading
-    fun leftHeading(): Heading
+class Heading(val cardinalPoint: CardinalPoint, val forwardDelta: Point2D, val backwardDelta: Point2D,
+              val rightCardinalPoint: CardinalPoint, val leftCardinalPoint: CardinalPoint) {
 
-    fun cardinalPoint(): CardinalPoint
+    fun cardinalPoint(): CardinalPoint = this.cardinalPoint
+    fun forwardPoint(): Point2D = this.forwardDelta
+    fun backwardPoint(): Point2D = this.backwardDelta
+    fun rightCardinalPoint(): CardinalPoint = this.rightCardinalPoint
+    fun leftCardinalPoint(): CardinalPoint = this.leftCardinalPoint
 }
--- src/main/kotlin/MarsRover.kt
+++ src/main/kotlin/MarsRover.kt
@@ -2,23 +2,22 @@ package dev.frtgh01
 
 import dev.frtgh01.Heading.CardinalPoint
 import dev.frtgh01.Heading.Heading
-import dev.frtgh01.Heading.HeadingEast
-import dev.frtgh01.Heading.HeadingNorth
-import dev.frtgh01.Heading.HeadingSouth
-import dev.frtgh01.Heading.HeadingWest
 
 class MarsRover(var position: Point2D, var heading: Heading) {
     companion object MarsRoverClass {
-        fun at(position: Point2D, heading: CardinalPoint): MarsRover {
-            val currentHeading : Heading = when (heading) {
-                CardinalPoint.NORTH -> HeadingNorth()
-                CardinalPoint.EAST -> HeadingEast()
-                CardinalPoint.SOUTH -> HeadingSouth()
-                CardinalPoint.WEST -> HeadingWest()
-            }
+        // Map with all the possible Headings
+        // where the key is the CardinalPoint for that Heading.
+        val headings: Map<CardinalPoint, Heading> = listOf(
+                Heading(CardinalPoint.NORTH, Point2D(0, 1), Point2D(0, -1), CardinalPoint.EAST, leftCardinalPoint = CardinalPoint.WEST),
+                Heading(CardinalPoint.EAST, Point2D(1, 0), Point2D(-1, 0), CardinalPoint.SOUTH, leftCardinalPoint = CardinalPoint.NORTH),
+                Heading(CardinalPoint.SOUTH, Point2D(0, -1), Point2D(0, 1), CardinalPoint.WEST, leftCardinalPoint = CardinalPoint.EAST),
+                Heading(CardinalPoint.WEST, Point2D(-1, 0), Point2D(1, 0), CardinalPoint.NORTH, leftCardinalPoint = CardinalPoint.SOUTH),
+            ).associateBy { heading -> heading.cardinalPoint() }
 
-            return MarsRover(position, currentHeading)
-        }
+        fun at(position: Point2D, cardinalPoint: CardinalPoint): MarsRover = MarsRover(position, headingFor(cardinalPoint))
+
+        fun headingFor(cardinalPoint: CardinalPoint): Heading = headings.getValue(cardinalPoint)
+
         fun invalidCommandErrorDescription(): String = "Invalid Command"
     }
 
@@ -30,8 +29,8 @@ class MarsRover(var position: Point2D, var heading: Heading) {
         when (command) {
             'f' -> this.position += this.heading.forwardPoint()
             'b' -> this.position += this.heading.backwardPoint()
-            'r' -> this.heading = this.heading.rightHeading()
-            'l' -> this.heading = this.heading.leftHeading()
+            'r' -> this.heading = headingFor(this.heading.rightCardinalPoint())
+            'l' -> this.heading = headingFor(this.heading.leftCardinalPoint())
             else -> throw InvalidCommandException(MarsRover.invalidCommandErrorDescription())
         }
     }
